f6a9ed68d39a1352d6e8046a8794840678c5ba53
Sam20180118 <140066525+Sam20180118@users.noreply.github.com>
2023-07-22 12:50:43 +0800

    Add files via upload

---
+++ src/utils/images.py
@@ -0,0 +1,24 @@
+# notes
+'''
+This file is used for handling anything image related.
+I suggest handling the local file encoding/decoding here as well as fetching any external images.
+'''
+
+# package imports
+import base64
+import os
+
+# # logo information
+# cwd = os.getcwd()
+# logo_path = os.path.join(cwd, 'assets', 'logos', 'logo_main.png')
+# logo_tunel = base64.b64encode(open(logo_path, 'rb').read())
+# logo_encoded = 'data:image/png;base64,{}'.format(logo_tunel.decode())
+
+cwd = os.getcwd()
+red_png = os.path.join(cwd, 'assets', 'image', 'red.jpg')
+red_base64 = base64.b64encode(open(red_png, 'rb').read()).decode('ascii')
+red_base64_encoded = 'data:image/png;base64,{}'.format(red_base64)
+
+blue_png = os.path.join(cwd, 'assets', 'image', 'blue.jpg')
+blue_base64 = base64.b64encode(open(blue_png, 'rb').read()).decode('ascii')
+blue_base64_encoded = 'data:image/png;base64,{}'.format(blue_base64)
